631d2c07c32d1af872a82a7b6818e26638e72006
Aamir Akwar Ali <aamirakwarali@Mac.lan>
2026-01-25 20:21:57 +0530

    Add proper spacer dividers between all home page sections

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,8 +4,14 @@ export default function HomePage() {
   return (
     <>
       <Hero />
+      {/* Spacer between Hero and Signature Dishes */}
+      <div className="h-16 md:h-24 bg-gradient-to-b from-black to-charcoal" />
       <SignatureDishes />
+      {/* Spacer between Signature Dishes and Customer Favorites */}
+      <div className="h-16 md:h-24 bg-gradient-to-b from-charcoal to-black" />
       <PopularBowls />
+      {/* Spacer between Customer Favorites and Dining Experience */}
+      <div className="h-16 md:h-24 bg-gradient-to-b from-black to-charcoal" />
       <DiningExperience />
       <CTASection />
     </>
