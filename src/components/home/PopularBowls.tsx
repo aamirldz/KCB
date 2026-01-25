12335b636141df233fe8294502c703c4811dcb74
Aamir Akwar Ali <aamirakwarali@Mac.lan>
2026-01-25 22:20:29 +0530

    Force section spacing with inline styles

--- src/components/home/PopularBowls.tsx
+++ src/components/home/PopularBowls.tsx
@@ -31,7 +31,7 @@ export default function PopularBowls() {
     };
 
     return (
-        <section className="mt-24 md:mt-32 lg:mt-40 py-24 bg-black relative overflow-hidden">
+        <section style={{ marginTop: '10rem' }} className="py-24 bg-black relative overflow-hidden">
             {/* Top Divider Line */}
             <div className="absolute top-0 left-0 right-0 h-px bg-gradient-to-r from-transparent via-gold/40 to-transparent" />
 
